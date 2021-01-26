a93e64984329f04365ae309296a281965e1d9bab
machida.kohei <kmachida12345@gmail.com>
2021-01-26 11:26:16 +0900

    Queryでparamを渡す 予報も取得

--- app/src/main/java/com/example/retrofitpractice/MainActivity.kt
+++ app/src/main/java/com/example/retrofitpractice/MainActivity.kt
@@ -1,15 +1,16 @@
 package com.example.retrofitpractice
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
-import com.example.retrofitpractice.model.WeatherResponse
+import com.example.retrofitpractice.model.ForecastInfo
+import com.example.retrofitpractice.model.WeatherInfo
 import kotlinx.coroutines.launch
-import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,30 +22,24 @@ class MainActivity : AppCompatActivity() {
                 .baseUrl("https://api.openweathermap.org")
                 .build().create(ApiInterface::class.java)
 
-        suspend fun getRequest(): WeatherResponse? {
-            return try {
-                val response: WeatherResponse = api.getWeather()
-                response
-            } catch (e: Exception) {
-                null
-            }
-        }
-
         lifecycleScope.launch {
-            val response = getRequest()
+            val response = api.getWeather(35.425007, 139.6658144, API_KEY)
             Log.d("hoge response ", response.toString())
+            val forecastRes = api.getWeatherForecast(35.425007, 139.6658144, API_KEY)
+            Log.d("hoge forecastRes ", forecastRes.toString())
         }
-
     }
 
     companion object {
         private const val API_KEY = "ccc66e80fccf4425a48eba9617272b73"
         private const val TAG = "MainActivity"
-        private const val API = "weather?lat={lat}&lon={lon}&appid={API key}"
     }
     interface ApiInterface {
-        @GET("/data/2.5/weather?lat=35.425007&lon=139.6658144&appid=ccc66e80fccf4425a48eba9617272b73")
-        suspend fun getWeather(): WeatherResponse
+        @GET("/data/2.5/weather/")
+        suspend fun getWeather(@Query("lat") lat: Double, @Query("lon") lon: Double, @Query("appid") appId: String): WeatherInfo
+
+        @GET("/data/2.5/forecast/")
+        suspend fun getWeatherForecast(@Query("lat") lat: Double, @Query("lon") lon: Double, @Query("appid") appId: String): ForecastInfo
     }
 
 }
